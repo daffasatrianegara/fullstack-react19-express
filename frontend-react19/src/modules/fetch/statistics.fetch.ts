48a5fb8db35a336fa9dc239b01957d3d815201f1
daffasatrianegara <daffasatria036@gmail.com>
2025-05-06 22:12:14 +0700

    feat: add modules fetch api for all api from backend

--- frontend-react19/src/modules/fetch/statistics.fetch.ts
+++ frontend-react19/src/modules/fetch/statistics.fetch.ts
@@ -1,1 +1,51 @@
 import { instance } from '../axios';
+
+const getCardInfoContent = async () => {
+    try {
+        const response = await instance.get('/statistics/card-info');
+        return response.data;
+    } catch (err: any) {
+        throw new Error(
+            err.response?.data.message ||
+                'API tidak tersambung (Internal server error).',
+        );
+    }
+};
+
+const getDataBarChart = async () => {
+    try {
+        const response = await instance.get('/statistics/users-cars');
+        return response.data;
+    } catch (err: any) {
+        throw new Error(
+            err.response?.data.message ||
+                'API tidak tersambung (Internal server error).',
+        );
+    }
+};
+
+const getDataPieChartGender = async () => {
+    try {
+        const response = await instance.get('/statistics/gender');
+        return response.data;
+    } catch (err: any) {
+        throw new Error(
+            err.response?.data.message ||
+                'API tidak tersambung (Internal server error).',
+        );
+    }
+};
+
+const getDataPieChartCarBrand = async () => {
+    try {
+        const response = await instance.get("/statistics/car-brand")
+        return response.data
+    } catch (err: any) {
+        throw new Error(
+            err.response?.data.message ||
+                'API tidak tersambung (Internal server error).',
+        );
+    }
+};
+
+export { getCardInfoContent, getDataBarChart, getDataPieChartGender, getDataPieChartCarBrand };
